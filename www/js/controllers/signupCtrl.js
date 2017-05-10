ae5f40cfe23547bd8cbb69942149e440b74e3317
Nagamurali <nagamuraliweb@gmail.com>
2017-05-10 22:28:58 +0530

    get user details function added

--- www/js/controllers/signupCtrl.js
+++ www/js/controllers/signupCtrl.js
@@ -12,20 +12,16 @@ angular.module('meapp.controllers.signupCtrl', [])
 		profession: ''
 	};
 
-	var userId = window.localStorage.getItem('userID');
-	switch (userId) {
-		case 1: $state.go('artist-register');
-				break;
-		case 2: $state.go('technicians-register');
-				break;
-		case 3: $state.go('clients-register');
-				break;
-		default: console.log('Login');
-	}
-
-	dataFactory.getType().then(function(resp) {
-		$scope.profession_types = JSON.parse(resp.data.types);
-	});
+	// var userId = window.localStorage.getItem('userID');
+	// switch (userId) {
+	// 	case 1: $state.go('artist-register');
+	// 			break;
+	// 	case 2: $state.go('technicians-register');
+	// 			break;
+	// 	case 3: $state.go('clients-register');
+	// 			break;
+	// 	default: console.log('Login');
+	// }
 
 	$scope.signup = function() {
 		loaderFactory.showLoader();
@@ -33,28 +29,16 @@ angular.module('meapp.controllers.signupCtrl', [])
 			loaderFactory.hideLoader();
 			if(resp.data.error === 1) {
 				loaderFactory.showAlert('Signup', resp.data.msg);
-				$scope.user = {
-					name: '',
-					email: '',
-					password: '',
-					confirm_password: '',
-					mobile: '',
-					profession: ''
-				};
-				$scope.form.signupForm.$setPristine();
-				$scope.form.signupForm.$setUntouched();
 				return;
 			} else {
 				window.localStorage.setItem('userID', resp.data.user_id);
-				switch (resp.data.user_id) {
-					case 1: $state.go('artist-register');
-							break;
-					case 2: $state.go('technicians-register');
-							break;
-					case 3: $state.go('clients-register');
-							break;
-					default: console.log('Signup');
-				}
+				var user_id = resp.data.user_id;
+				console.log(user_id);
+				dataFactory.getUserDetails(resp.data.user_id).then(function(resp) {
+					console.log(resp);
+					//$scope.profession_types = JSON.parse(resp.data.types);
+				});
+				
 			}
 
 		});
