3e499636b5b23cb47640071f9f80716cd3649987
Nagamurali <nagamuraliweb@gmail.com>
2017-05-10 22:35:43 +0530

    gettype fixed

--- www/js/controllers/signupCtrl.js
+++ www/js/controllers/signupCtrl.js
@@ -23,12 +23,26 @@ angular.module('meapp.controllers.signupCtrl', [])
 	// 	default: console.log('Login');
 	// }
 
+	dataFactory.getType().then(function(resp) {
+		$scope.profession_types = JSON.parse(resp.data.types);
+	});
+
 	$scope.signup = function() {
 		loaderFactory.showLoader();
 		loginFactory.signUp($scope.user).then(function(resp) {
 			loaderFactory.hideLoader();
 			if(resp.data.error === 1) {
 				loaderFactory.showAlert('Signup', resp.data.msg);
+				$scope.user = {
+					name: '',
+					email: '',
+					password: '',
+					confirm_password: '',
+					mobile: '',
+					profession: ''
+				};
+				$scope.form.signupForm.$setPristine();
+				$scope.form.signupForm.$setUntouched();
 				return;
 			} else {
 				window.localStorage.setItem('userID', resp.data.user_id);
