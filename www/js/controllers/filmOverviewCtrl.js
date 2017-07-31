dbacaf86090e33a4d6c8bca05ddc6a1739de19aa
Magesh K <mgshk.1507@gmail.com>
2017-07-31 19:34:15 +0530

    code optimised

--- www/js/controllers/filmOverviewCtrl.js
+++ www/js/controllers/filmOverviewCtrl.js
@@ -4,9 +4,9 @@
 	angular.module('meapp.controllers.filmOverviewCtrl', [])
 	.controller('filmOverviewCtrl', filmOverviewCtrl);
 
-	filmOverviewCtrl.$inject = ['$scope', 'dataFactory', 'artistFactory', 'loaderFactory', '$state'];
+	filmOverviewCtrl.$inject = ['$scope', 'dataFactory', 'artistFactory', 'loaderFactory', '$state', 'coreConstant'];
 
-	function filmOverviewCtrl($scope, dataFactory, artistFactory, loaderFactory, $state) {
+	function filmOverviewCtrl($scope, dataFactory, artistFactory, loaderFactory, $state, coreConstant) {
 
 		var vm = this;
 		$scope.user_id = window.localStorage.getItem('userID');
@@ -14,47 +14,14 @@
 
 		dataFactory.getUserDetails($scope.user_id).then(function(resp) {
 			vm.client = JSON.parse(resp.data.user_details);
-		});
 
-		dataFactory.getBody().then(function(resp) {
-			var bodies = JSON.parse(resp.data.bodies);
-			vm.body = bodies[vm.client.user_body_id];
+			vm.body = coreConstant.bodies[vm.client.user_body_id];
+			vm.project = coreConstant.projectType[vm.client.user_project_id];
+			vm.role = coreConstant.roles[vm.client.user_role_id];
+			vm.profession = coreConstant.type[vm.client.user_type];
+			vm.looking = coreConstant.genders[vm.client.user_looking_for];
+			vm.exp = coreConstant.experience[vm.client.user_experience_id];
+			vm.lang = coreConstant.languages[vm.client.user_language_id];
 		});
-
-		dataFactory.getProjects().then(function(resp) {
-			var projects = JSON.parse(resp.data.projects);
-			vm.project = projects[vm.client.user_project_id];
-		});
-
-		dataFactory.getRoles().then(function(resp) {
-			var roles = JSON.parse(resp.data.roles);
-			vm.role = roles[vm.client.user_role_id];
-		});
-
-		dataFactory.getType().then(function(resp) {
-			var profession_types = JSON.parse(resp.data.types);
-			vm.profession = profession_types[vm.client.user_type];
-		});
-
-		dataFactory.getGender().then(function(resp) {
-			var genders = JSON.parse(resp.data.genders);
-			vm.gender = genders[vm.client.user_gender_id];
-		});
-
-		dataFactory.getGender().then(function(resp) {
-			var genders1 = JSON.parse(resp.data.genders);
-			vm.looking = genders1[vm.client.user_looking_for];
-		});
-
-		dataFactory.getExperience().then(function(resp) {
-			var experience = JSON.parse(resp.data.experiences);
-			vm.exp = experience[vm.client.user_experience_id];
-		});
-
-		dataFactory.getLanguages().then(function(resp) {
-			var languages = JSON.parse(resp.data.languages);
-			vm.lang = languages[vm.client.user_language_id];
-		});
-
 	}
 })();
--- www/js/services/dataService.js
+++ www/js/services/dataService.js
@@ -1,14 +1,13 @@
 angular.module('meapp.services.dataService', [])
 	.service('dataService', ['$http', function($http) {
-
-	this.getType = function() {
+    
+    this.getFilterUsers = function(user_type) {
         var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getType',
+            method: 'GET',
+            url: 'http://mewhoelse.in/api/api.php?f=getFilterUsers&filter_user='+user_type,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
+            }
         };
 
         return $http(request).then(function (resp) {
@@ -16,14 +15,13 @@ angular.module('meapp.services.dataService', [])
         });
     }
 
-    this.getBody = function() {
+    this.getProjectDetails = function(project_type) {
         var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getBody',
+            method: 'GET',
+            url: 'http://mewhoelse.in/api/api.php?f=getProjectDetails&project_type='+project_type,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
+            }
         };
 
         return $http(request).then(function (resp) {
@@ -31,119 +29,13 @@ angular.module('meapp.services.dataService', [])
         });
     }
 
-    this.getExperience = function() {
+    this.getClientPosts = function(user_id) {
         var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getExperience',
+            method: 'GET',
+            url: 'http://mewhoelse.in/api/api.php?f=getClientPosts&user_id='+user_id,
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getHairs = function() {
-        var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getHairs',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getHairColors = function() {
-        var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getHairColors',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getLanguages = function() {
-        var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getLanguages',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getSkills = function() {
-        var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getSkills',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getSkins = function() {
-        var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getSkins',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getProjects = function() {
-        var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getProjects',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getRoles = function() {
-        var request = {
-            method: 'POST',
-            url: 'http://mewhoelse.in/api/api.php?f=getRoles',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {}
+            }
         };
 
         return $http(request).then(function (resp) {
@@ -155,34 +47,6 @@ angular.module('meapp.services.dataService', [])
         var request = {
             method: 'GET',
             url: 'http://mewhoelse.in/api/api.php?f=getUserDetails&user_id='+user_id,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getUsersDetails = function() {
-        var request = {
-            method: 'GET',
-            url: 'http://mewhoelse.in/api/api.php?f=getUsersDetails',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        };
-
-        return $http(request).then(function (resp) {
-            return resp;
-        });
-    }
-
-    this.getGender = function() {
-        var request = {
-            method: 'GET',
-            url: 'http://mewhoelse.in/api/api.php?f=getGender',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
