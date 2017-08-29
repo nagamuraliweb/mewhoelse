2a22ce40a90408f367591f0fd58f52436d60277f
Magesh K <mgshk.1507@gmail.com>
2017-08-29 22:40:40 +0530

    film overview page

--- www/js/controllers/filmOverviewCtrl.js
+++ www/js/controllers/filmOverviewCtrl.js
@@ -9,6 +9,8 @@
 	function filmOverviewCtrl($scope, dataFactory, artistFactory, loaderFactory, $state, coreConstant) {
 
 		var vm = this;
+		var languages = [];
+
 		$scope.user_id = window.localStorage.getItem('userID');
 		$scope.version = new Date().getTime();
 
@@ -22,6 +24,15 @@
 			vm.looking = coreConstant.genders[vm.client.user_looking_for];
 			vm.exp = coreConstant.experience[vm.client.user_experience_id];
 			vm.lang = coreConstant.languages[vm.client.user_language_id];
+
+			angular.forEach(vm.client.user_language_id.split(','), function(key) {
+				languages.push(coreConstant.languages[key]);
+			});
+
+			if(vm.client.user_language_others)
+				languages.push(vm.client.user_language_others);
+
+			vm.lang = languages.join(', ');
 		});
 	}
 })();
